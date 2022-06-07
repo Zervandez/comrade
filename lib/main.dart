527e8f413e55f222ff9e905038d8283e85340604
Zervandez <arielhalilaj@live.com>
2022-06-07 15:38:11 +0200

    k

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,24 @@
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:mysql1/mysql1.dart';
 
 Future<void> main() async {
   runApp(const MyApp());
-
-  // Open a connection (testdb should already exist)
-  MySqlConnection.connect(ConnectionSettings(
-          host: 'localhost',
-          port: 3306,
-          user: 'root',
-          password: 'Haffenstadt!',
-          db: 'dbco'))
-      .then((conn) {
-    conn.query('select * from User').then((results) {
-      List.from(results).forEach((row) {
-        print('UserName: ${row[0]}');
-        print('object');
-      });
-      // closing database connection here
-    });
-    conn.close();
-  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // In here, we check if the user is logged in and redirect accordingly
+
+  Future<String> getAllUsers() async {
+    var response = await http.get(Uri.parse('http://10.0.0.2:8000/users'));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception("Ya lost, bobby!");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +28,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
-          child: Text('Halmmmmmlo'),
-        ),
+            child: FutureBuilder<String>(
+                future: getAllUsers(),
+                builder: (context, snapshot) {
+                  print(snapshot.data);
+                  if (snapshot.hasData && snapshot.data != null) {
+                    //return Text(snapshot.data);
+                  }
+                  return CircularProgressIndicator();
+                })),
       ),
     );
   }
