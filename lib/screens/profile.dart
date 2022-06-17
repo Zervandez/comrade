daeeead65b6d196bd626c46c60cab19424a2a1cb
Zervandez-Theke <arielh96@icloud.com>
2022-06-17 16:39:35 +0200

    assets

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -32,10 +32,84 @@ class UserProfile extends StatelessWidget {
                   fontWeight: FontWeight.w100,
                 ),
               ),
-            )
+            ),
+
+            //horizontal hobbies list
+            // kur te lidhim backendin do bahen bashke kta
+            Container(
+              height: 40,
+              margin: EdgeInsets.all(8),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem(),
+                  hobbyItem()
+                ],
+              ),
+            ),
+
+            Container(
+              height: 240,
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: Colors.black,
+              )),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  singleChartColumn(),
+                  singleChartColumn(),
+                  singleChartColumn(),
+                  singleChartColumn(),
+                  singleChartColumn(),
+                  singleChartColumn(),
+                  singleChartColumn()
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+Widget hobbyItem() => Container(
+      margin: EdgeInsets.all(4),
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white70,
+          border: Border.all(
+            color: Colors.deepPurple,
+            width: 2,
+          )),
+      child: Text("Yogamano"),
+    );
+
+Widget singleChartColumn() => Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          margin: EdgeInsets.all(8),
+          width: 30,
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.amber,
+          ),
+        ),
+        Text('M'),
+      ],
+    );
