55fb869dbc5a7c5c76a70f82adb852f228093ebf
Zervandez-Theke <arielh96@icloud.com>
2022-06-20 10:44:45 +0200

    theke data re

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
                   if (snapshot.hasData && snapshot.data != null) {
                     //return Text(snapshot.data);
                   }
-                  return CircularProgressIndicator();
+                  return const CircularProgressIndicator();
                 })),
       ),
     );
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-void main() => runApp(UserProfile());
+void main() => runApp(const UserProfile());
 // The main() class is here for debbuging purposes
 
 class UserProfile extends StatelessWidget {
@@ -9,77 +10,107 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           leading: const Icon(Icons.flag_sharp),
-          title: const Text("Frankfurt"),
-          actions: const [
-            Text("M26"),
-          ],
-        ),
-        body: Column(
-          children: [
-            Container(
-              child: Image.asset("aa"),
+          title: const Text(
+            "Frankfurt",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
             ),
+          ),
+          actions: [
             Container(
-              padding: const EdgeInsets.all(12),
               alignment: Alignment.center,
-              child: const Text(
-                "@ag",
+              margin: EdgeInsets.all(12),
+              child: Text(
+                "M60",
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w100,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-
-            //horizontal hobbies list
-            // kur te lidhim backendin do bahen bashke kta
-            Container(
-              height: 40,
-              margin: EdgeInsets.all(8),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem(),
-                  hobbyItem()
-                ],
-              ),
-            ),
-
-            Container(
-              height: 240,
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                color: Colors.black,
-              )),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  singleChartColumn(),
-                  singleChartColumn(),
-                  singleChartColumn(),
-                  singleChartColumn(),
-                  singleChartColumn(),
-                  singleChartColumn(),
-                  singleChartColumn()
-                ],
-              ),
-            ),
           ],
+        ),
+        body: Container(
+          // height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: 180.0,
+                width: 180.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/sabba_profile_pic.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(12),
+                alignment: Alignment.center,
+                child: const Text(
+                  "@makaronenFresser999",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+              ),
+
+              //horizontal hobbies list
+              // kur te lidhim backendin do bahen bashke kta
+              Container(
+                height: 40,
+                margin: const EdgeInsets.all(8),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem(),
+                    hobbyItem()
+                  ],
+                ),
+              ),
+
+              Container(
+                alignment: Alignment.bottomCenter,
+                height: 240,
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.black,
+                )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    singleChartColumn(),
+                    singleChartColumn(),
+                    singleChartColumn(),
+                    singleChartColumn(),
+                    singleChartColumn(),
+                    singleChartColumn(),
+                    singleChartColumn()
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -87,8 +118,8 @@ class UserProfile extends StatelessWidget {
 }
 
 Widget hobbyItem() => Container(
-      margin: EdgeInsets.all(4),
-      padding: EdgeInsets.all(5),
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(5),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: Colors.white70,
@@ -96,20 +127,20 @@ Widget hobbyItem() => Container(
             color: Colors.deepPurple,
             width: 2,
           )),
-      child: Text("Yogamano"),
+      child: const Text("Italianer sein"),
     );
 
 Widget singleChartColumn() => Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
-          margin: EdgeInsets.all(8),
+          margin: const EdgeInsets.all(8),
           width: 30,
           height: 120,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Colors.amber,
           ),
         ),
-        Text('M'),
+        Text('Mi'),
       ],
     );
