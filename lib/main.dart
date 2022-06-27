e97b0566db312db2ce9378c308ded7ef677abe25
Zervandez <arielhalilaj@live.com>
2022-06-27 10:10:17 +0200

    fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
-import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // In here, we check if the user is logged in and redirect accordingly
-
-  Future<String> getAllUsers() async {
-    var response = await http.get(Uri.parse('http://10.0.0.2:8000/users'));
-
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception("Ya lost, bobby!");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,16 +26,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: Center(
-            child: FutureBuilder<String>(
-                future: getAllUsers(),
-                builder: (context, snapshot) {
-                  print(snapshot.data);
-                  if (snapshot.hasData && snapshot.data != null) {
-                    //return Text(snapshot.data);
-                  }
-                  return const CircularProgressIndicator();
-                })),
+        body: Center(child: FutureBuilder<String>(builder: (context, snapshot) {
+          print(snapshot.data);
+          if (snapshot.hasData && snapshot.data != null) {
+            //return Text(snapshot.data);
+          }
+          return const CircularProgressIndicator();
+        })),
       ),
     );
   }
